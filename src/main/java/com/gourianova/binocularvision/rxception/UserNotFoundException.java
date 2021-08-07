2c4b765c23f5d32394960cdb06f0f502b2f96293
Tatiana-Gourianova <62667085+Tatiana-Gourianova@users.noreply.github.com>
2021-08-08 02:22:37 +0300

    Delete src/main/java/com/gourianova/binocularvision/rxception directory

--- src/main/java/com/gourianova/binocularvision/rxception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.gourianova.binocularvision.rxception;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
